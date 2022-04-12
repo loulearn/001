bd298039d2e444d8749221e60249e24d53cd85fc
greg <louholcomb@gmail.com>
2022-04-12 13:04:51 -0500

    ignore

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -4,4 +4,11 @@ if (module.hot) {
   module.hot.accept()
 }
 
-/* example below */
+/* lesson 42 example code below */
+
+function person(name, favColor) {
+  console.log("Hello my name is " + name + " and my favorite color is " + favColor + ".");
+}
+
+person("Jon D", "blu");
+person("Jan", "gr");
