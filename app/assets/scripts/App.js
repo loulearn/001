1e8fceb2d0502c1f7ac35bc0d29763ec717f281e
greg <louholcomb@gmail.com>
2022-04-12 16:47:15 -0500

    43

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,8 +1,16 @@
 import '../styles/styles.css'
+import Person from './modules/Person'
 
 if (module.hot) {
   module.hot.accept()
 }
 
 /* lesson 42 example code below */
+
+
+var john = new Person("Jo Doe", "blue");
+john.greet();
+
+var jane = new Person("Jane Smith", "brown");
+jane.greet();
 
