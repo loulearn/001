25a1f8a6eb85cdabf9b2c0b7ed32ae5368205811
greg <louholcomb@gmail.com>
2022-04-12 13:07:38 -0500

    practice with js

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -6,9 +6,3 @@ if (module.hot) {
 
 /* lesson 42 example code below */
 
-function person(name, favColor) {
-  console.log("Hello my name is " + name + " and my favorite color is " + favColor + ".");
-}
-
-person("Jon D", "blu");
-person("Jan", "gr");
