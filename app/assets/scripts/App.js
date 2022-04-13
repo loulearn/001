27cee7ad001ae53d60ea3df251bb3d65158e5fdf
greg <louholcomb@gmail.com>
2022-04-12 23:40:57 -0500

    first part of 44

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,21 +1,8 @@
 import '../styles/styles.css'
-import Person from './modules/Person'
+import MobileMenu from './modules/MobileMenu'
+
+let mobileMenu = new MobileMenu();
 
 if (module.hot) {
   module.hot.accept()
 }
-
-/* lesson 42 example code below */
-class Adult extends Person {
-  payTaxes() {
-    console.log(this.name + " now owes zero taxes.");
-  }
-}
-
-var john = new Person("Joe Doe", "y");
-john.greet();
-
-var jane = new Adult("Jane Smith", "e");
-jane.greet();
-jane.payTaxes();
-
