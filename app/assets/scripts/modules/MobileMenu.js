8442a9ebca612096d7e662901636d3eb88c88c87
greg <louholcomb@gmail.com>
2022-04-13 16:08:35 -0500

    add from end of vid 44 - mobile menu toggle on/off

--- app/assets/scripts/modules/MobileMenu.js
+++ app/assets/scripts/modules/MobileMenu.js
@@ -1,6 +1,7 @@
 class MobileMenu {
     constructor() {
         this.menuIcon = document.querySelector(".site-header__menu-icon")
+        this.menuContent = document.querySelector(".site-header__menu-content")
         this.events()
     }
 
@@ -9,8 +10,9 @@ class MobileMenu {
     }
 
     toggleTheMenu() {
-        console.log("The icon was clicked")
+        this.menuContent.classList.toggle("site-header__menu-content--is-visible")
     }
 }
 
 export default MobileMenu;
+
